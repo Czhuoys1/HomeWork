bc5dde8e2322d57a1d243620edae03ae56bc9e4e
luofan <3523118228@qq.com>
2025-06-09 12:27:21 +0800

    小米商城购物车实现

---
+++ HomeWork/任务3-1-小米商城购物车的设计与实现/js/script.js
@@ -0,0 +1,11 @@
+const goodsList =  [
+    { "id": 1, "name": "手机", "price": 5999, "count": 1, "chacked": false,"img": './img/phone.png' },
+    { "id": 2, "name": "平板", "price": 3999, "count": 1, "chacked": false,"img": './img/pad.png' },
+    { "id": 3, 'name': "电脑", "price": 8999, "count": 1, "chacked": false,"img": './img/intel.png' },
+    { "id": 4, "name": "耳机", "price": 2399, "count": 1, "chacked": false,"img": './img/pods.png' },
+    { "id": 5, "name": "手表", "price": 1999, "count": 1, "chacked": false,"img": './img/watch.png' },
+    { "id": 6, "name": "手环", "price": 599, "count": 1, "chacked": false,"img": './img/shouhuan.png'},
+]
+
+
+export { goodsList };
